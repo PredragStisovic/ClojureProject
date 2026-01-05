b00f8560871e2b706c7f8445c43e07fd576d777d
PredragStisovic <stisovicpredrag@gmail.com>
2026-01-05 13:29:32 +0100

    Refactored user score calculation, modified configuration files

--- src/should_user_run.clj
+++ src/should_user_run.clj
@@ -2,13 +2,17 @@
   (:require [user-data]
            [weather]))
 
-(defn should-run [conditions]
-  (>= (apply * (map val conditions)) 0.5))
+(defn score-description [score]
+  (cond
+    (<= score 0.3) "It is not suggested to run today, as both weather conditions and your latest running data show you need some rest"
+    (<= score 0.6) "We recommend you go take it easy if you want to run today"
+    (<= score 0.8) "It is recommended you go on a short run today"
+    :else "You are good to go, the weather is good and you are in an excellent condition to run!"))
 
 (defn return-run-message [body]
-  (let [weather-score 1
-        user-score (user-data/get-user-score body)]
-    (if (should-run {:weather weather-score
-                     :user    user-score})
-      "You are in great condition to go for a run"
-      "It is recommended that you dont run at this moment")))
+  (let [weather-score (weather/calculate-weather-score (:latitude body) (:longitude body))
+        user-score (user-data/get-user-score body)
+        combined (+ (* weather-score 0.6)
+                    (* user-score 0.4))]
+    {:score combined
+     :text-description (score-description combined)}))
--- src/weather.clj
+++ src/weather.clj
@@ -1,49 +1,54 @@
 (ns weather
-  (:require [clj-http.client :as http]))
-  (require '[aero.core :refer [read-config]])
-(def config (read-config "config.edn"))
+  (:require
+    [clj-http.client :as http]
+    [aero.core :refer [read-config]]))
 
-(def api-key (:api-key config))
-(def base-url (:base-url config))
-
-(defn get-current-weather [location]
-  (let [url (str base-url "/current.json")
-        response (http/get url {:query-params {:key api-key :q location} :as :json})]
+(defn get-current-weather [latitude longitude]
+  (let [config (read-config "config.edn")
+        api-key (:api-key config)
+        base-url (:base-url config)
+        url (str base-url "/current.json")
+        response (http/get url
+                           {:query-params {:key api-key
+                                           :q (str latitude "," longitude)}
+                            :as :json})]
     (:body response)))
 
-(defn calculate-running-score
-  [{:keys [feelslike_c precip_mm gust_kph vis_km]}]
+(defn calculate-weather-score
+  [latitude longitude]
+  (let [current-weather (:current (get-current-weather latitude longitude))
+        feelslike_c (:feelslike_c current-weather)
+        precip_mm (:precip_mm current-weather)
+        gust_kph (:gust_kph current-weather)
+        vis_km (:vis_km current-weather)
 
-  (let [temp-score
+        temp-score
         (cond
+          (<= feelslike_c 0) 0
           (>= feelslike_c 35) 0
-          (<= feelslike_c 10) 0
-          (< feelslike_c 5)   0.5
-          (> feelslike_c 25)  0.5
-          :else               1)
+          (<= feelslike_c 10) 0.2
+          (>= feelslike_c 25) 0.5
+          :else 1)
 
         precipitation-score
         (cond
-          (> precip_mm 1.0) 0
-          (> precip_mm 0.5) 0.5
-          :else             1)
+          (> precip_mm 2.0) 0
+          (> precip_mm 1.0) 0.3
+          (> precip_mm 0.5) 0.6
+          :else 1)
 
         wind-score
         (cond
           (>= gust_kph 50) 0
-          (>= gust_kph 25) 0.5
-          :else            1)
+          (>= gust_kph 30) 0.3
+          (>= gust_kph 15) 0.6
+          :else 1)
 
         visibility-score
         (cond
           (< vis_km 1) 0
           (< vis_km 2) 0.5
-          :else        1)
-        total (+ temp-score precipitation-score wind-score visibility-score)
-        weighted-res (cond
-          (<= total 1.5) 0
-          (<= total 2.5) 0.5
           :else 1)
-        ]
-    weighted-res
-    ))
+
+        total (/ (+ temp-score precipitation-score wind-score visibility-score) 4.0)]
+    total))
